23fad08ac2780ed6e1da082b2faaf7dbc9eea014
SheldenR <bbshelden@gmail.com>
2026-05-26 21:16:05 -0400

    Added hyperlink for why I created vrtue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="creator-section">
         <div className="creator-container">
           <div className="creator-content">
-            <div className="hero-label-static">/ Why <a href="https://www.sheldenr.dev/blog-posts/04-28-26-21" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'none', borderBottom: '1px solid currentColor' }}>I created VRTUE</a></div>
+            <div className="hero-label-static">/ Why I created VRTUE</div>
             <p className="creator-description">
               <a href="https://www.sheldenr.dev/blog-posts/04-28-26-21" target="_blank" rel="noopener noreferrer" className="inline-link">I created VRTUE</a> because at some point I wanted to look for an inspiration in tech and, looking for <span className="highlight">people like me</span> I realized there is genuinely very few who are motivated to the degree I was early in my college career. Looking at YC founders, only around <span className="highlight">4% of YC founders are black.</span> I wanted to <span className="highlight">aggregate the best of black talent</span> to provide the inspiration for people in similar scenarios as myself.
             </p>
