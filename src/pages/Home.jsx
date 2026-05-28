32ac5237944aa49ca3c4cb6b96a328329c0cf075
SheldenR <bbshelden@gmail.com>
2026-05-27 23:26:23 -0400

    Updated description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import seanImage from '../assets/sean-tolliver.jpg';
 import jermaineImage from '../assets/jermaine-washington.jpg';
 
 const talentData = [
-  { id: 5, name: 'Jermaine Washington Jr.', location: 'Atlanta, USA', role: 'Senior Software Engineer at Equifax.', socialLink: 'https://www.linkedin.com/in/jermainewashingtonjr/', image: jermaineImage, aspectRatio: '1.1/1' },
+  { id: 5, name: 'Jermaine Washington Jr.', location: 'Atlanta, USA', role: 'Senior Software Engineer at Equifax. Florida State University Alumni.', socialLink: 'https://www.linkedin.com/in/jermainewashingtonjr/', image: jermaineImage, aspectRatio: '1.1/1' },
   { id: 4, name: 'Sean Tolliver', location: 'Atlanta, USA', role: 'Co-Founder of TRKGLOBAL LLC. Georgia Tech Alumni MBA Management Of Technology.', socialLink: 'https://www.linkedin.com/in/seantolliver/', image: seanImage, aspectRatio: '1/1' },
   { id: 1, name: 'Uyiosa Nehikhuere', location: 'Atlanta, USA', role: 'Software Engineer Intern @ New York Life, Prev SWE Intern @ FactSet, Computer Science student at Georgia State University.', socialLink: 'https://www.linkedin.com/in/uyiosa-nehikhuere-16406328b/', image: uyiosaImage, aspectRatio: '1/1' },
   { id: 3, name: 'Raphael Omorose', location: 'Atlanta, USA', role: '2x Software Engineer Intern @ Uber, Computer Science student at Georgia State University, GSU Colorstack Vice President', socialLink: 'https://www.linkedin.com/in/raphaelomorose/', image: raphaelImage, aspectRatio: '1.2/1' },
