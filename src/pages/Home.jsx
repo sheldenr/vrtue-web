ebc85b27d4bcb36aae1c726a9071de9764183037
SheldenR <bbshelden@gmail.com>
2026-06-25 11:48:25 -0400

    Added Jianni Adkisson

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,8 +16,10 @@ import georgeImage from '../assets/george-purnell.webp';
 import rolandImage from '../assets/roland-saavedra.webp';
 import koladeImage from '../assets/kolade-ayeni.webp';
 import khalilImage from '../assets/khalil-sayid.webp';
+import jianniImage from '../assets/jianni-adkisson.webp';
 
 const talentData = [
+  { id: 18, name: 'Jianni Adkisson', location: 'Atlanta, Georgia', role: 'Software Engineer III @ Google', socialLink: 'https://www.linkedin.com/in/jianniadkisson/', image: jianniImage, aspectRatio: '0.9/1' },
   { id: 17, name: 'Khalil Sayid', location: 'Atlanta, Georgia', role: 'Software Engineer at Microsoft. Georgia Tech Alumni.', socialLink: 'https://www.linkedin.com/in/khalil-sayid/', image: khalilImage, aspectRatio: '1/1' },
   { id: 16, name: 'Kolade Ayeni', location: 'Seattle, Washington', role: 'Software Engineer at Microsoft', socialLink: 'https://www.linkedin.com/in/kolade-ayeni/', image: koladeImage, aspectRatio: '0.86/1' },
   { id: 15, name: 'Roland Saavedra', location: 'San Francisco, California', role: 'Co-Founder of MarkIt (YC F25). Prev at Google.', socialLink: 'https://www.linkedin.com/in/rsaav/', image: rolandImage, aspectRatio: '1/1', isYC: true },
